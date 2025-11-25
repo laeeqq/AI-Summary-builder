4a8b1d58b1896014ddfb3fa356bb122d73753c36
Laeeq <lladh918@mtroyal.ca>
2025-11-24 22:31:42 -0700

    initial coding started

---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+// Starts the web Server 
+// COnfiguring how the API behaves
+//sets up the routes (basically our main like in other languages)
+
+import express from "express"; // the framework to help us run the logic and handle the requests
+
+const app = express(); // server object
+
